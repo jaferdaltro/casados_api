35a32d4be16012b1eb349e544ae56a129c9db023
Jafer Daltro <jafer.daltro@gmail.com>
2024-11-12 08:37:25 -0300

    change fields name

--- db/migrate/20241111001722_add_fields_to_users.rb
+++ db/migrate/20241111001722_add_fields_to_users.rb
@@ -2,7 +2,7 @@ class AddFieldsToUsers < ActiveRecord::Migration[7.2]
   def change
     add_column :users, :cpf, :string
     add_column :users, :gender, :string
-    add_column :users, :date_of_birth, :date
-    add_column :users, :shirt_size, :string
+    add_column :users, :birth_at, :date
+    add_column :users, :tshirt_size, :string
   end
 end
