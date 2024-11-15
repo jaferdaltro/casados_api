b51231e89bb018bf08bd9ac493ee8175782511b9
Jafer Daltro <jafer.daltro@gmail.com>
2024-11-15 20:18:02 -0300

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,14 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resource :registrations, only: [ :create ]
   namespace :api, constraints: { format: "json" } do
     namespace :v1 do
+      resource :vouchers, only: [ :create ]
+      namespace :student do
+        resource :subscriptions, only: [ :create ]
+      end
       namespace :user do
+        resource :registrations, only: [ :create ]
         resource :sessions, only: [ :create, :destroy ]
       end
     end
