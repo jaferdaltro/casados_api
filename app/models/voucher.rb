7849b00385b346199e9cc00e0626d833a9344e27
Jafer Daltro <jafer.daltro@gmail.com>
2024-11-20 16:30:34 -0300

    Fix voucher validate

--- app/models/voucher.rb
+++ app/models/voucher.rb
@@ -1,7 +1,7 @@
 class Voucher < ApplicationRecord
   validates :code, presence: true, uniqueness: true
 
-  def is_valide?(voucher)
+  def self.is_valide?(voucher)
     voucher.is_available && voucher.expiration_at >= DateTime.now
   end
 end
