1aa148322df89f54bf9d16e208e4a18f8cb17a0a
Jafer Daltro <jafer.daltro@gmail.com>
2024-11-20 14:33:18 -0300

    Improve method marriage

--- app/models/marriage.rb
+++ app/models/marriage.rb
@@ -6,16 +6,10 @@ class Marriage < ApplicationRecord
 
   def self.create_marriage(husband_params, wife_params, marriage_params)
     transaction do
-      husband = User.new(husband_params)
-      wife = User.new(wife_params)
-      if husband.save && wife.save
-        husband_id = husband.id
-        wife_id = wife.id
-      else
-       errors.add(:user, "algua coisa errada no usuário")
-      end
-      marriage = ::Marriage.new(husband_id: husband_id,
-                                wife_id: wife_id,
+      husband = User.create!(husband_params)
+      wife = User.create!(wife_params)
+      marriage = ::Marriage.new(husband_id: husband.id,
+                                wife_id: wife.id,
                                 is_member: marriage_params[:is_member],
                                 registred_by: marriage_params[:registred_by],
                                 reason: marriage_params[:reason],
@@ -25,5 +19,7 @@ class Marriage < ApplicationRecord
                               )
       marriage.save!
     end
+  rescue ActiveRecord::RecordInvalid => e
+    Rails.logger.error("Erro ao criar o casamento: #{e.message}")
   end
 end
