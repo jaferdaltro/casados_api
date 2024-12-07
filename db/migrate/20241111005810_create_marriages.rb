880012a2f1c85ea785e51b77f0761d852c30569b
Jafer Daltro <jafer.daltro@gmail.com>
2024-12-07 10:48:51 -0300

    Add flag for dinner

--- db/migrate/20241111005810_create_marriages.rb
+++ db/migrate/20241111005810_create_marriages.rb
@@ -4,6 +4,7 @@ class CreateMarriages < ActiveRecord::Migration[7.2]
       t.integer :husband_id
       t.integer :wife_id
       t.string :registered_by
+      t.boolean :dinner_participation, default: false
       t.text :reason
       t.boolean :is_member
       t.string :campus
--- db/schema.rb
+++ db/schema.rb
@@ -31,6 +31,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_11_170837) do
     t.integer "husband_id"
     t.integer "wife_id"
     t.string "registered_by"
+    t.boolean "dinner_participation", default: false
     t.text "reason"
     t.boolean "is_member"
     t.string "campus"
