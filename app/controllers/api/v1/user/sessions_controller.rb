0968be15eaa858ed99d34f37d1b186afc4778d8d
Jafer Daltro <jafer.daltro@gmail.com>
2025-01-10 23:21:23 -0300

    remove reset_session

--- app/controllers/api/v1/user/sessions_controller.rb
+++ app/controllers/api/v1/user/sessions_controller.rb
@@ -4,8 +4,8 @@ module API::V1
       def create
         user = ::User.find_by(cpf: params[:session][:cpf])
         if user && user.authenticate(params[:session][:password])
-          reset_session
-          remember user
+          # reset_session
+          # remember user
           log_in user
           render json: { user: user }, status: :ok
         else
