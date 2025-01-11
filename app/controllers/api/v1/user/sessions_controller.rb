76046c156421770261be9bb7959206a4a9df4c63
Jafer Daltro <jafer.daltro@gmail.com>
2025-01-10 23:31:57 -0300

    remove hook

--- app/controllers/api/v1/user/sessions_controller.rb
+++ app/controllers/api/v1/user/sessions_controller.rb
@@ -1,6 +1,6 @@
 module API::V1
     class User::SessionsController < BaseController
-      skip_before_action :authenticate_user!
+      # skip_before_action :authenticate_user!
       def create
         user = ::User.find_by(cpf: params[:session][:cpf])
         if user && user.authenticate(params[:session][:password])
